6484ebb3ce0d1af66c30aa2f2a6679b1ad7b64d2
leeyamkeng <leeyamkeng120@gmail.com>
2017-11-22 10:48:18 +0800

    version 1.1

---
+++ app/demomode.py
@@ -0,0 +1,10 @@
+import os
+from shutil import copyfile
+
+
+os.system('sudo pkill -9 -f harpu.py')
+copyfile('/home/pi/demo/demo.py', '/home/pi/harpu.py')
+#time.sleep(2)
+os.system('lxterminal -e python /home/pi/harpu.py')
+demomessage = "Machine Now in Demo Mode"
+print "Done"
