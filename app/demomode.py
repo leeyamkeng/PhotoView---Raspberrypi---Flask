5acc7af3301c0bac88ba052e30b338f72a2dfb29
leeyamkeng <leeyamkeng120@gmail.com>
2017-11-29 22:38:43 +0800

    delete time sleep

--- app/demomode.py
+++ app/demomode.py
@@ -4,7 +4,6 @@ from shutil import copyfile
 
 os.system('sudo pkill -9 -f harpu.py')
 copyfile('/home/pi/demo/demo.py', '/home/pi/harpu.py')
-#time.sleep(2)
 os.system('lxterminal -e python /home/pi/harpu.py')
 demomessage = "Machine Now in Demo Mode"
 print ("Done")
